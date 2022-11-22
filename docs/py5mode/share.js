d9933a701e0f11a389e88dec491ff7ec22725da6
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-11-22 18:47:19 -0300

    Update share.js

--- docs/py5mode/share.js
+++ docs/py5mode/share.js
@@ -21,13 +21,13 @@ function decodeSketchUrl(encodedSketch) {
 
 function checkForSketch() {
   let initialSketch = `def setup():
-    createCanvas(200, 200)
+    size(400, 400)
 
 def draw():
     background(200)
-    diameter = sin(frameCount / 60) * 50 + 50
+    diameter = sin(frame_count / 60) * 50 + width / 2
     fill("blue")
-    ellipse(100, 100, diameter, diameter)
+    ellipse(width / 2, height / 2, diameter, diameter)
     `;
 
   const currentUrl = new URLSearchParams(window.location.search);
