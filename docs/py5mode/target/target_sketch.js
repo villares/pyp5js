96fdc03344fb6585ba9948ed4ff714ae8a36da6d
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-10-06 10:48:12 -0300

    remove p5.js max and min (use Python's)
    p5.js max and min can't handle iterables and where "shadowing" Python's built-ins

--- docs/py5mode/target/target_sketch.js
+++ docs/py5mode/target/target_sketch.js
@@ -715,12 +715,6 @@ def mag(*args):
 def remap(*args):
     return _P5_INSTANCE.map(*args)
 
-def max(*args):
-    return _P5_INSTANCE.max(*args)
-
-def min(*args):
-    return _P5_INSTANCE.min(*args)
-
 def norm(*args):
     return _P5_INSTANCE.norm(*args)
 
