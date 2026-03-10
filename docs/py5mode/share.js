c6178fa4c64939480935d24c4296243e8a5bdbfc
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-03-10 17:34:05 -0300

    changing starting example (empty)

--- docs/py5mode/share.js
+++ docs/py5mode/share.js
@@ -25,9 +25,6 @@ function checkForSketch() {
 
 def draw():
     background(200)
-    diameter = sin(frame_count / 60) * 50 + width / 2
-    fill("blue")
-    ellipse(width / 2, height / 2, diameter, diameter)
     `;
 
   const currentUrl = new URLSearchParams(window.location.search);
