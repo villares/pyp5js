cc0c134541322055547e28c081bf6c4817d71b9e
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2026-03-10 17:46:06 -0300

    removing more from the starting example

--- docs/py5mode/share.js
+++ docs/py5mode/share.js
@@ -22,9 +22,6 @@ function decodeSketchUrl(encodedSketch) {
 function checkForSketch() {
   let initialSketch = `def setup():
     size(400, 400)
-
-def draw():
-    background(200)
     `;
 
   const currentUrl = new URLSearchParams(window.location.search);
